071632f76895c1bbd386ad6ea7be10ca705ed0f8
CarlosCastro1979 <carlos.alexandre.castro@gmail.com>
2026-05-13 20:39:24 -0300

    Update sw (1).js

--- sw (1).js
+++ sw (1).js
@@ -1,17 +1,1 @@
-// Service Worker — limpa cache e auto-destrói
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.registration.unregister())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Sem cache — vai sempre à rede
-  e.respondWith(fetch(e.request));
-});
+// deprecated
